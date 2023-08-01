5c138d803fa8f0e047bdc4ac1be0a9e89c44f26a
Ivana <ivana.gyro@gmail.com>
2023-08-01 23:25:14 +0800

    Use BackendSampler to create sampler for Fujitsu
    `qiskit_ibm_runtime.Sampler` and `qiskit_ibm_runtime.Service` cannot
    consume the backend provided by Fujitsu, and `qiskit.primitives.Sampler`
    seems doesn't trigger the backend passed to it.

--- qmps/loschmidts/real_dev_submit/20230711/src/qite_run_time_script_fujitsu.py
+++ qmps/loschmidts/real_dev_submit/20230711/src/qite_run_time_script_fujitsu.py
@@ -1,6 +1,6 @@
-from qiskit_ibm_runtime import Sampler, Options
-from qiskit_qulacs import QulacsProvider
 from mpi4py import MPI
+from qiskit_qulacs import QulacsProvider
+from qiskit.primitives import BackendSampler
 
 import qite_qiskit
 
@@ -8,9 +8,6 @@ import qite_qiskit
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-
 backend = QulacsProvider().get_backend()  # Get QulacsBackend instance
-options = Options(optimization_level=2)
-sampler = Sampler(options=options, backend=backend)
+sampler = BackendSampler(backend=backend)
 qite_qiskit.main(sampler)
-
